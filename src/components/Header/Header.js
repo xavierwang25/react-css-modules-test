414ba51b8d92bcf6810f1f4719925a346cca5f72
richard255999 <richard255999@protonmail.com>
2020-05-31 11:00:11 -0400

    Add header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 
 const Header = () => {
     return (
-        <div className={styles.header}>Tickets</div>
+        <div className={styles.title}>Tickets</div>
     )
 }
 
