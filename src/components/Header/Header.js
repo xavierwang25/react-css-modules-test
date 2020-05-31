9e73533397e44b1cad5ee68afe904a877795c00f
henry25225 <henry25225@protonmail.com>
2020-05-31 10:52:21 -0400

    Define color system

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={styles.header}>Tickets</div>
+    )
+}
+
+export default Header;
