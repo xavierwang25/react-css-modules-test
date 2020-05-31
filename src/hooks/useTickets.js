de9712c27d8a70a291886d4d36b32671b74b33be
henry25225 <henry25225@protonmail.com>
2020-05-31 12:04:28 -0400

    Add sidebar and integrate api

---
+++ src/hooks/useTickets.js
@@ -0,0 +1,7 @@
+import useSWR from 'swr'
+import axios from 'axios'
+
+export default () => {
+  const { data } = useSWR('https://raw.githubusercontent.com/Tapify/public-code-test/master/web-ui-test/tickets.json', axios.get)
+  return data ? data.data : [];
+}
