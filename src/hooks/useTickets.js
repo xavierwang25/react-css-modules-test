254dbfcc86b7808699f66def498b3c3f68060283
xavierwang25 <xavierwang25@protonmail.com>
2020-05-31 13:26:07 -0400

    Refactor sidebar to ticket list

--- src/hooks/useTickets.js
+++ src/hooks/useTickets.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 
 export default () => {
   const { data } = useSWR('https://raw.githubusercontent.com/Tapify/public-code-test/master/web-ui-test/tickets.json', axios.get)
-  return data ? data.data : [];
+  return data ? data.data : null;
 }
