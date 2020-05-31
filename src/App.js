844a68db930f889230e02fee6cc8e38b349a9a04
richard255999 <richard255999@protonmail.com>
2020-05-31 12:04:28 -0400

    Add sidebar and integrate api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import styles from './App.module.css';
+import { useMemoOne } from 'use-memo-one';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar'
+import useTickets from './hooks/useTickets'
+import styles from './App.module.css';
 
 const App = () => {
+  const tickets = useTickets();
+
+  const sTickets = useMemoOne(() => tickets ? tickets.map(ticket => ({
+    id: ticket.ticketId,
+    avatar: ticket.owner.avatar,
+    reportedTime: ticket.reportedTime,
+    status: ticket.status,
+    asset: ticket.asset.name
+  })) : [], tickets)
+
   return (
     <div className={styles.container}>
       <Header></Header>
+      <div className={styles.body}>
+        <Sidebar tickets={sTickets}></Sidebar>
+      </div>
     </div>
   );
 }
