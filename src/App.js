87508338cbfb3dd8bf36c61054b36497772c038f
henry25225 <henry25225@protonmail.com>
2020-05-31 10:34:39 -0400

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const styles = require('./App.module.css')
+
+const App = () => {
+  return (
+    <div className={styles.container}>asdfasdf</div>
+  );
+}
+
+export default App;
