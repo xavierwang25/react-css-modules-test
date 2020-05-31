c5312efae7c7854a6d73e3f3aad014ee2bb8410a
yin12559 <yin12559@protonmail.com>
2020-05-31 15:11:46 -0400

    Add layout of ticket detail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useMemoOne } from 'use-memo-one';
+import _ from 'lodash';
 import Header from './components/Header';
 import TicketList from './components/TicketList'
+import TicketDetail from './components/TicketDetail'
 import useTickets from './hooks/useTickets'
 import styles from './App.module.css';
 import insensitiveIncludes from './utils/insensitiveIncludes';
@@ -9,6 +11,7 @@ import insensitiveIncludes from './utils/insensitiveIncludes';
 const App = () => {
   const tickets = useTickets();
   const [filter, setFilter] = useState('');
+  const [selectedTicketId, setSelectedTicketId] = useState(null);
 
   const ticketList = useMemoOne(() => tickets ? tickets
     .filter(ticket => insensitiveIncludes(ticket.reportedTime, filter)
@@ -24,6 +27,19 @@ const App = () => {
       status: ticket.status,
       asset: ticket.asset.name
     })) : [], [tickets, filter])
+  
+  const ticketDetail = useMemoOne(() => {
+    if (!selectedTicketId) {
+      return null;
+    }
+    const ticket = _.find(tickets, {ticketId: selectedTicketId});
+    return {
+      ..._.pick(['number', 'lastUpdatedTime', 'specialities']),
+      name: `${ticket.owner.firstName} ${ticket.owner.lastName}`,
+      assetName: ticket.asset.name,
+      geoCode: ticket.asset.geoCode
+    }
+  }, [tickets, setSelectedTicketId]);
 
   return (
     <div className={styles.container}>
@@ -31,10 +47,13 @@ const App = () => {
       <div className={styles.main}>
         <div className={styles.sidebar}>
           <div className={styles.search}>
-            <i class="fas fa-search"></i>
+            <i className="fas fa-search"></i>
             <input className={styles.searchInput} value={filter} onChange={e => setFilter(e.target.value)}></input>
           </div>
-          <TicketList tickets={ticketList}></TicketList>
+          <TicketList tickets={ticketList} selectedId={selectedTicketId} onTicketClick={id => setSelectedTicketId(id)}></TicketList>
+        </div>
+        <div className={styles.content}>
+          <TicketDetail ticket={ticketDetail}></TicketDetail>
         </div>
       </div>
     </div>
